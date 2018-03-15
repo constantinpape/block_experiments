594a555d1f6d6f7788a6f59cb2a3058969e448bc
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-03-15 16:51:55 -0400

    Fix issues in blockwise seg

--- segmentation_scripts/blockwise_segmentations.py
+++ segmentation_scripts/blockwise_segmentations.py
@@ -1,7 +1,12 @@
+import json
 import pickle
+import time
 from concurrent import futures
 from functools import partial
 
+# the good old anti-segfault vigra import ...
+import vigra
+import numpy as np
 import nifty
 import nifty.graph.rag as nrag
 import z5py
@@ -9,19 +14,33 @@ import cremi_tools.segmentation as cseg
 
 
 def segment_block(path, out_key, blocking, block_id, halo, segmenter):
+    print("Processing block", block_id, "/", blocking.numberOfBlocks)
+    t0 = time.time()
     f = z5py.File(path)
     block = blocking.getBlockWithHalo(block_id, halo)
 
+    # all bounding boxes
     bb = tuple(slice(start, stop)
                for start, stop in zip(block.outerBlock.begin, block.outerBlock.end))
+    inner_bb = tuple(slice(start, stop)
+                     for start, stop in zip(block.innerBlock.begin, block.innerBlock.end))
+    local_bb = tuple(slice(start, stop)
+                     for start, stop in zip(block.innerBlockLocal.begin, block.innerBlockLocal.end))
 
+    # load mask
+    mask = f['masks/min_filter_mask'][bb]
+    # if we only have mask, continue
+    if np.sum(mask) == 0:
+        print("Finished", block_id, "; contained only mask")
+        return time.time() - t0
+
+    # load affinities and convert them to proper format
     affs = f['predictions/full_affs'][(slice(None),) + bb]
-    # TODO need to cast affinities to float ?!
-    # TODO need to invert affinities
+    if affs.dtype == np.dtype('uint8'):
+        affs = affs.astype('float32') / 255.
+    affs = 1. - affs
 
-    # TODO
-    mask = f['masks/'][bb]
-
+    # make watershed
     wslr = cseg.LRAffinityWatershed(threshold_cc=0.1, threshold_dt=0.2, sigma_seeds=2.,
                                     size_filter=50)
     ws, max_label = wslr(affs, mask)
@@ -29,11 +48,9 @@ def segment_block(path, out_key, blocking, block_id, halo, segmenter):
     segmentation = segmenter(ws, affs, max_label + 1)
     ds_out = f[out_key]
 
-    inner_bb = tuple(slice(start, stop)
-                     for start, stop in zip(block.innerBlock.begin, block.innerBlock.end))
-    local_bb = tuple(slice(start, stop)
-                     for start, stop in zip(block.innerBlockLocal.begin, block.innerBlockLocal.end))
     ds_out[inner_bb] = segmentation[local_bb].astype('uint64')
+    print("Finished", block_id)
+    return time.time() - t0
 
 
 def segment_mc(ws, affs, n_labels, offsets):
@@ -41,7 +58,7 @@ def segment_mc(ws, affs, n_labels, offsets):
     probs = nrag.accumulateAffinityStandartFeatures(rag, affs, offsets, numberOfThreads=1)[:, 0]
     uv_ids = rag.uvIds()
 
-    mc = cseg.Multicut('kernighan-lin')
+    mc = cseg.Multicut('kernighan-lin', weight_edges=False)
     costs = mc.probabilities_to_costs(probs)
     ignore_edges = (uv_ids == 0).any(axis=1)
     costs[ignore_edges] = 5 * costs.min()
@@ -49,7 +66,7 @@ def segment_mc(ws, affs, n_labels, offsets):
     graph = nifty.graph.undirectedGraph(n_labels)
     graph.insertEdges(uv_ids)
     node_labels = mc(graph, costs)
-    return nrag.projectScalarNodeLabelsToPixels(rag, node_labels)
+    return nrag.projectScalarNodeDataToPixels(rag, node_labels)
 
 
 def segment_mcrf(ws, affs, offsets, rf):
@@ -65,15 +82,17 @@ def segment_lmcrf(ws, affs, offsets, rf_local, rf_lifted):
     pass
 
 
+# TODO refactor into function
 if __name__ == '__main__':
-    path = ''
+    path = '/nrs/saalfeld/lauritzen/02/workspace.n5/filtered'
+    f = z5py.File(path)
 
     chunk_shape = (25, 256, 256)
     # TODO maybe use 1k blocks (factor 4) ?!
     block_shape = tuple(cs * 2 for cs in chunk_shape)
     halo = [5, 50, 50]
 
-    shape = z5py.File(path)['raw'].shape
+    shape = f['gray'].shape
     blocking = nifty.tools.blocking(roiBegin=[0, 0, 0],
                                     roiEnd=list(shape),
                                     blockShape=list(block_shape))
@@ -83,13 +102,27 @@ if __name__ == '__main__':
                [-3, 0, 0], [0, -9, 0], [0, 0, -9],
                [-4, 0, 0], [0, -27, 0], [0, 0, -27]]
     segmenter = partial(segment_mc, offsets=offsets)
-    n_threads = 40
+    n_threads = 60
 
-    out_key = 'segmentations/mc_aff'
-    f = z5py.File(path, use_zarr_format=False)
-    f.create_dataset(out_key, shape=shape, chunk_shape=chunk_shape, compression='gzip', dtype='uint64')
+    group = 'segmentations'
+    # CHANGE FOR DIFFERENT SEGMENTERS
+    key = 'mc_affs_not_stitched'
+    if group not in f:
+        f.create_group(group)
 
+    out_key = '%s/%s' % (group, key)
+    if out_key not in f:
+        f.create_dataset(out_key, shape=shape, chunks=chunk_shape,
+                         compression='gzip', dtype='uint64')
+
+    t0 = time.time()
     with futures.ThreadPoolExecutor(n_threads) as tp:
         tasks = [tp.submit(segment_block, path, out_key, blocking, block_id, halo, segmenter)
                  for block_id in range(blocking.numberOfBlocks)]
-        [t.result() for t in tasks]
+        results = [t.result() for t in tasks]
+    # results = [segment_block(path, out_key, blocking, block_id, halo, segmenter)
+    #            for block_id in range(blocking.numberOfBlocks)]
+    print("Total processing time:", time.time() - t0)
+
+    with open('./timings_02_%s.json' % key, 'w') as ft:
+        json.dump(results, ft)
