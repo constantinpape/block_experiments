4d6b898adfab74be3458f37cbab389ef90a8dc42
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-03-25 11:05:42 -0400

    Update viewer script

--- synapse_scripts/view_syn.py
+++ synapse_scripts/view_syn.py
@@ -4,9 +4,9 @@ from cremi_tools.viewer.volumina import view
 
 
 def view_synapses(bb):
-    raw_path = '/nrs/saalfeld/lauritzen/02/workspace.n5/filtered'
+    raw_path = '/home/papec/mnt/nrs/lauritzen/02/workspace.n5/filtered'
     raw_key = 'gray'
-    syn_path = '/nrs/saalfeld/lauritzen/02/workspace.n5'
+    syn_path = '/home/papec/mnt/nrs/lauritzen/02/workspace.n5'
     syn_key = 'syncleft_dist_DTU-2_200000'
 
     raw = z5py.File(raw_path)[raw_key][bb]
@@ -16,5 +16,5 @@ def view_synapses(bb):
 
 
 if __name__ == '__main__':
-    bb = np.s_[1000:1100, 2000:2512, 2000:2512]
+    bb = np.s_[400:450, 2000:2512, 2000:2512]
     view_synapses(bb)
