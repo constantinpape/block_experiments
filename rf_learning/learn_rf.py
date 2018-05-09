6ce3ed0818dab739d671846ae5aaf38ed81de3b7
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-05-09 15:13:13 -0400

    Update random forest learning

--- rf_learning/learn_rf.py
+++ rf_learning/learn_rf.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import numpy as np
 import nifty.graph.rag as nrag
@@ -6,7 +7,11 @@ import vigra
 from sklearn.ensemble import RandomForestClassifier
 
 
-# TODO
+FEATURES = ('default_affs',
+            'separate_affs',
+            'glia')
+
+
 def affinity_features(rag, affs):
     offsets = [[-1, 0, 0], [0, -1, 0], [0, 0, -1],
                [-2, 0, 0], [0, -3, 0], [0, 0, -3],
@@ -16,11 +21,26 @@ def affinity_features(rag, affs):
     return np.nan_to_num(features)
 
 
+def separate_channel_features(rag, affs):
+    offsets = [[-1, 0, 0], [0, -1, 0], [0, 0, -1],
+               [-2, 0, 0], [0, -3, 0], [0, 0, -3],
+               [-3, 0, 0], [0, -9, 0], [0, 0, -9],
+               [-4, 0, 0], [0, -27, 0], [0, 0, -27]]
+    assert len(affs) == len(offsets)
+    features = []
+    for channel_id in range(len(affs)):
+        features.append(nrag.accumulateAffinityStandartFeatures(rag,
+                                                                affs[channel_id:channel_id+1],
+                                                                [offsets[channel_id]]))
+    features = np.concatenate(features, axis=1)
+    return np.nan_to_num(features)
+
+
 def glia_features(rag, seg, glia):
     uv_ids = rag.uvIds()
-    statistics = ["Mean", "Count", "Kurtosis", "Maximum",
-                  "Minimum", "Quantiles", "RegionRadii",
-                  "Skewness", "Sum", "Variance"]
+    # FIXME for some reason 'Quantiles' are not working
+    statistics = ["Mean", "Variance", "Skewness", "Kurtosis",
+                  "Minimum", "Maximum", "Count", "RegionRadii"]
     extractor = vigra.analysis.extractRegionFeatures(glia, seg.astype('uint32'),
                                                      features=statistics)
 
@@ -41,33 +61,48 @@ def glia_features(rag, seg, glia):
 
 
 def edge_labels(rag, gt):
-    uv_ids = rag.uv_ids()
+    uv_ids = rag.uvIds()
     node_labels = nrag.gridRagAccumulateLabels(rag, gt)
     edge_labels = (node_labels[uv_ids[:, 0]] != node_labels[uv_ids[:, 1]]).astype('uint8')
     edge_mask = (node_labels[uv_ids] != 0).all(axis=1)
+    print(np.sum(edge_mask), "edges of", len(uv_ids), "are valid")
     return edge_labels, edge_mask
 
 
-def learn_rf(paths, save_path):
+def learn_rf(paths, save_path, feature_keys):
+    assert feature_keys
+    assert all(feat in FEATURES for feat in feature_keys)
     all_features = []
     all_labels = []
     for path in paths:
+        print("Computing features and labels for", path)
         f = z5py.File(path)
         seg = f['volumes/labels/watershed'][:]
         rag = nrag.gridRag(seg, numberOfLabels=int(seg.max()) + 1)
 
         affs = f['volumes/predictions/affinities'][:]
         if affs.dtype == np.dtype('uint8'):
+            print("Cast affinities to float")
             affs = affs.astype('float32')
             affs /= 255.
-        glia = affs[-1:]
-        affs = 1. - affs[:-1]
+        glia = affs[-1]
+        # affs = 1. - affs[:-1]
+        affs = affs[:-1]
+        print(affs.min(), affs.max())
 
-        features = np.concatenate([affinity_features(rag, affs),
-                                   glia_features(rag, seg, glia)],
-                                  axis=1)
+        features = []
+        if 'default_affs' in feature_keys:
+            features.append(affinity_features(rag, affs))
+        if 'separate_affs' in feature_keys:
+            features.append(separate_channel_features(rag, affs))
+        if 'glia' in feature_keys:
+            features.append(glia_features(rag, seg, glia))
 
+        features = np.concatenate(features, axis=1)
+
+        mask = f['volumes/labels/mask'][:]
         gt = f['volumes/labels/neuron_ids'][:]
+        gt[np.logical_not(mask)] = 0
         labels, edge_mask = edge_labels(rag, gt)
         assert features.shape[0] == labels.shape[0]
 
@@ -80,14 +115,22 @@ def learn_rf(paths, save_path):
 
     rf = RandomForestClassifier(n_jobs=40, n_estimators=200)
     rf.fit(all_features, all_labels)
+    rf.n_jobs = 1
     with open(save_path, 'wb') as f:
         pickle.dump(rf, f)
 
 
 if __name__ == '__main__':
-    save_path = ''
-    samples = ['0', '1', '2', 'A', 'B', 'C']
-    # TODO
-    paths = ['%s.n5' % sample
+    train_folder = '/nrs/saalfeld/papec/cremi2.0/training_data/V1_20180419'
+
+    features = ['default_affs']
+    features.sort()
+
+    feat_str = '_'.join(features)
+    save_path = os.path.join(train_folder, 'rf_%s.pkl' % feat_str)
+
+    samples = ['0', '1', '2_', 'A', 'B', 'C_']
+    paths = [os.path.join(train_folder, 'n5', 'sample%s.n5' % sample)
              for sample in samples]
-    learn_rf(samples, save_path)
+
+    learn_rf(paths, save_path, features)
